6cddbd4fdeb058306273fdedcceaf62bf0a29e12
Heijung <heijung.jang@hufs.ac.kr>
2018-11-06 13:10:05 +0900

    시간 새로고침 기능 구현

--- app/src/main/java/heijung/ledcontrol/TimeActivity.java
+++ app/src/main/java/heijung/ledcontrol/TimeActivity.java
@@ -3,6 +3,8 @@ package heijung.ledcontrol;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -10,13 +12,19 @@ import java.util.Date;
 import java.util.Locale;
 
 public class TimeActivity extends AppCompatActivity{
-
     long now = System.currentTimeMillis();
     Date date = new Date(now);
     SimpleDateFormat sdfNow = new SimpleDateFormat("HH:mm", Locale.KOREA);
     String formatDate = sdfNow.format(date);
 
     TextView dateNow;
+    Button RefreshBtn;
+
+    private String getTime(){
+        now = System.currentTimeMillis();
+        date = new Date(now);
+        return sdfNow.format(date);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,5 +33,22 @@ public class TimeActivity extends AppCompatActivity{
 
         dateNow = (TextView)findViewById(R.id.dateNow);
         dateNow.setText(formatDate);
+
+        RefreshBtn = (Button)findViewById(R.id.refreshBtn);
+
+        RefreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.refreshBtn:
+                        dateNow.setText(getTime());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
+
+
 }
